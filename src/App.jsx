29f4700a8e767e883ec9884041052269b4d38926
mihirrprajapati <work.mihir19@gmail.com>
2024-05-21 23:47:07 +0530

    Create Header and Footer for Social Media App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from "./component/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "./App.css";
+import Footer from "./component/Footer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
